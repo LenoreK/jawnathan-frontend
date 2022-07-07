95ca50f1572fe29e45f7dbbf11adf9edd7e0da63
Lenore Kelly <lenorekelly2@gmail.com>
2022-07-07 13:10:07 -0400

    edited the footer so it takes up less space

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,25 @@
 import { FaFacebook } from "react-icons/fa"
 import { FaInstagram } from "react-icons/fa"
 import { FaYoutube } from "react-icons/fa"
+import { Nav } from 'react-bootstrap';
 
 function Footer() {
     return (
-        <footer>
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item" href="/Contact">Contact</li>
-                <div class="nav-item" className="social">
-                    <a className="facebook" href="https://www.facebook.com/john.sellers.129"><FaFacebook/></a>
-                    <a className="instagram" href="https://instagram.com/jawn_sellers?igshid=YmMyMTA2M2Y="><FaInstagram/></a>
-                    <a className="youtube" href="https://www.youtube.com/channel/UCCJlod4ApI_lpHFZYVbyDJg"><FaYoutube/></a>
+            <small><Nav className="navbar-nav mr-auto" id="footer">
+                <Nav.Link className="nav-item" id="nav-item" href="/Copyright">&copy; Copyright 2022 Jawnathan Sellers</Nav.Link>
+                <div id="nav-item">
+                    <Nav.Link href="/Contact">Contact</Nav.Link>
+                    <div className="nav-item" id="social">
+                        <a className="facebook" href="https://www.facebook.com/john.sellers.129"><FaFacebook/></a>
+                        <a className="instagram" href="https://instagram.com/jawn_sellers?igshid=YmMyMTA2M2Y="><FaInstagram/></a>
+                        <a className="youtube" href="https://www.youtube.com/channel/UCCJlod4ApI_lpHFZYVbyDJg"><FaYoutube/></a>
+                    </div>
                 </div>
-                <li class="nav-item" href="">&copy; Copyright 2022 Jawnathan Sellers</li>
-                <li class="nav-item" href="">Privacy Policy</li>
-            </ul>
-        </footer>
+                <div id="nav-item">
+                    <Nav.Link id="music" className="nav-item" href="/Music">Music</Nav.Link>
+                    <Nav.Link className="nav-item" id="privacy" href="/Privacy">Privacy Policy</Nav.Link>
+                </div>
+            </Nav></small>
     )
 }
 
