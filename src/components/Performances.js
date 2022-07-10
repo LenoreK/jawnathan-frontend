9c2f588cf1343194a86e11a51c11b42429d424d8
Lenore Kelly <lenorekelly2@gmail.com>
2022-07-10 14:01:21 -0400

    change code

--- src/components/Performances.js
+++ src/components/Performances.js
@@ -1,7 +1,71 @@
+import Button  from 'react-bootstrap/Button';
+
 function Performances() {
     return (
-        <div>
+        <div className="performances">
+            <Button>Create New Performance</Button>
+            {/* <div className="first-performance">
+                <div className="date">
+                    <h3>{data.performance.date}</h3>
+                </div>
+                <div className="location">
+                    <div className="restaurant-time">
+                        <h3>Located in {data.performance.place} @ {data.performance.start_time} - {data.performance.end_time}</h3>
+                    </div>
+                    <div className="city-state">
+                        <h3>{data.performance.city}, {data.performance.state}</h3>
+                    </div>
+                </div>
+                <a href={`${data.performance.url}`}><Button>Website</Button></a>
+            </div> */}
+            <div className="second-performance">
+                <div className="date">
+                    <h1>date</h1>
+                </div>
+                <div className="location">
+                    <div className="restaurant-time">
+                        <h1>resturant / time</h1>
+                    </div>
+                    <div className="city-state">
+                        <h1>city / state</h1>
+                    </div>
+                </div>
+                <Button>Website</Button>   
+            </div>
+            <div className="third-performance">
+                <div className="date">
+                    <h1>date</h1>
+                </div>
+                <div className="location">
+                    <div className="restaurant-time">
+                        <h1>resturant / time</h1>
+                    </div>
+                    <div className="city-state">
+                        <h1>city / state</h1>
+                    </div>
+                </div>
+                <Button>Website</Button>   
+            </div>
+            <div className="fourth-performance">
+                <div className="date">
+                    <h1>date</h1>
+                </div>
+                <div className="location">
+                    <div className="restaurant-time">
+                        <h1>resturant / time</h1>
+                    </div>
+                    <div className="city-state">
+                        <h1>city / state</h1>
+                    </div>
+                </div>
+                <Button>Website</Button>   
+            </div>
+            <div className="calendar-div">
+                <h2>Calendar</h2>
+                <div className="calendar">
 
+                </div>
+            </div>
         </div>
     )
 }
